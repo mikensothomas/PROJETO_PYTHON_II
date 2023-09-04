0975ecffe6f9b5e266963bd4e4e4586b9dcd2763
Mikenson <thomasmikenson40@gmail.com>
2023-09-04 11:25:53 -0300

    commit

--- empresa/app_empresa/models.py
+++ empresa/app_empresa/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 class Usuario(models.Model):
     id = models.AutoField(primary_key=True)
-    funcionarios = models.IntegerField()
+    funcionarios = models.IntegerField(blank=True, null=True)
+    clientes = models.IntegerField(blank=True, null=True)
+    fornecedores = models.IntegerField(blank=True, null=True)
+    produtos = models.IntegerField(blank=True, null=True)
+    empresas_terceiras = models.IntegerField(blank=True, null=True)
--- empresa/app_empresa/views.py
+++ empresa/app_empresa/views.py
@@ -7,6 +7,10 @@ def home(request):
 def usuarios(request):
     novo_usuario = Usuario()
     novo_usuario.funcionarios = request.POST.get('funcionarios')
+    novo_usuario.clientes = request.POST.get('clientes')
+    novo_usuario.fornecedores = request.POST.get('fornecedores')
+    novo_usuario.produtos = request.POST.get('produtos')
+    novo_usuario.empresas_terceiras = request.POST.get('empresas_terceiras')
     novo_usuario.save()
 
     usuarios = {
