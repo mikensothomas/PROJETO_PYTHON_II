18574c687b1d767e2bdd27a2e53a72677bf55939
Mikenson <thomasmikenson40@gmail.com>
2023-09-08 20:45:08 -0300

    c

--- empresa/app_empresa/models.py
+++ empresa/app_empresa/models.py
@@ -2,14 +2,11 @@ from django.db import models
 
 class Usuario(models.Model):
     id = models.AutoField(primary_key=True)
-    funcionarios = models.IntegerField(blank=True, null=True)
+    funcionarios = models.IntegerField(default=0)
+    bt1mais = models.IntegerField(default=0)
     clientes = models.IntegerField(blank=True, null=True)
     fornecedores = models.IntegerField(blank=True, null=True)
     produtos = models.IntegerField(blank=True, null=True)
     empresas_terceiras = models.IntegerField(blank=True, null=True)
 
-
-def aumentar(self):
-        valor = models.IntegerField(default=0)
-        self.valor += 1
-        self.save()
+    
